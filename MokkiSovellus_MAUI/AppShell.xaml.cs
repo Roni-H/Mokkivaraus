e5065d29b118d3b57d428f9a30222a1064fa7807
Roni <roni.hagman@gmail.com>
2026-03-30 13:20:57 +0300

    NET MAUI

---
+++ MokkiSovellus_MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MokkiSovellus_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MokkiSovellus_MAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MokkiSovellus_MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MokkiSovellus_MAUI.Pages")]
