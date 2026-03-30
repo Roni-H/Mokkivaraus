baaf435b43d61896e3fa67fc5bbe2e24b557c725
Roni <roni.hagman@gmail.com>
2026-03-30 13:48:20 +0300

    Muutoksia

--- MokkiSovellus_MAUI/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MokkiSovellus_MAUI.AssemblyInfo.cs
+++ MokkiSovellus_MAUI/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MokkiSovellus_MAUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MokkiSovellus_MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+57a63bcbc776dabdb28f7d733b75f8d5c2e7f9d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ffb1e3feeedee8693d1f575daddf012019f0fb65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MokkiSovellus_MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MokkiSovellus_MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
